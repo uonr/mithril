ca79f6557afcc528581cc30d703235e48bb4821e
Tachibana Kiyomi <me@yuru.me>
2022-08-22 10:58:04 +0900

    Symbol

--- home.nix
+++ home.nix
@@ -136,6 +136,32 @@
 
   programs.starship = {
     enable = true;
+    settings = {
+      rust = {
+        symbol = "𝓡 ";
+      };
+      nix_shell = {
+        symbol = "❆ ";
+        disabled = true;
+      };
+      lua = {
+        symbol = "☽";
+      };
+      nodejs = {
+        symbol = "⬢ ";
+        disabled = true;
+      };
+      python = {
+        symbol = "Py ";
+      };
+      directory = {
+        read_only = " [RO]";
+        read_only_style = "yellow";
+      };
+      terraform = {
+        disabled = true;
+      };
+    };
   };
 
   # Let Home Manager install and manage itself.
